f85e5fe47662bbcec845a42d1ec220485a9f3259
dilan <dilantorrezsalinas@gmail.com>
2025-12-31 12:26:11 -0400

    contributions movido a un nuevo microservicio dedicado Contributions-Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,15 @@ import { DatabaseModule } from './database/database.module';
 import { LoanProceduresModule } from './loan_procedures/loan_procedures.module';
 import { LoanModalityParametersModule } from './loan_modality_parameters/loan_modality_parameters.module';
 import { LoanInterestsModule } from './loan_interests/loan_interests.module';
-import { ContributionsModule } from './contributions/contributions.module';
 
 @Module({
   imports: [
     CommonModule,
     DatabaseModule,
-    ConfigModule,
+    ConfigModule.forRoot(),
     LoanProceduresModule,
     LoanModalityParametersModule,
     LoanInterestsModule,
-    ContributionsModule,
   ],
   controllers: [],
   providers: [],
